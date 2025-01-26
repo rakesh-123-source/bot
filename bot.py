1ec00983dec76252ec09403d659dc5302119ec5c
rakesh-123-source <radheshyaam.gazol.1111@gmail.com>
2025-01-27 03:01:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def send_daily_messages():
         promotion_time_11pm = datetime.combine(today, datetime.min.time()) + timedelta(hours=23)
         if now > promotion_time_11pm:
             promotion_time_11pm += timedelta(days=1)
-        promotion_time_2am = datetime.combine(today, datetime.min.time()) + timedelta(hours=3)
+        promotion_time_2am = datetime.combine(today, datetime.min.time()) + timedelta(hours=3,minutes=4)
         if now > promotion_time_2am:
             promotion_time_2am += timedelta(days=1)
         promotion_time_5am = datetime.combine(today, datetime.min.time()) + timedelta(hours=6)
