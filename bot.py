7da8b62d0791914a0117ce8fdb0d7cc2ce106783
rakesh-123-source <radheshyaam.gazol.1111@gmail.com>
2025-01-28 22:54:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,25 +48,13 @@ async def send_daily_messages():
         promotion_time_12pm = datetime.combine(today, datetime.min.time()) + timedelta(hours=12)
         if now > promotion_time_12pm:
             promotion_time_12pm += timedelta(days=1)
-        promotion_time_3pm = datetime.combine(today, datetime.min.time()) + timedelta(hours=15)
-        if now > promotion_time_3pm:
-            promotion_time_3pm += timedelta(days=1)
         promotion_time_7pm = datetime.combine(today, datetime.min.time()) + timedelta(hours=19)
         if now > promotion_time_7pm:
             promotion_time_7pm += timedelta(days=1)
-        promotion_time_11pm = datetime.combine(today, datetime.min.time()) + timedelta(hours=23)
-        if now > promotion_time_11pm:
-            promotion_time_11pm += timedelta(days=1)
-        promotion_time_2am = datetime.combine(today, datetime.min.time()) + timedelta(hours=2)
-        if now > promotion_time_2am:
-            promotion_time_2am += timedelta(days=1)
-        promotion_time_5am = datetime.combine(today, datetime.min.time()) + timedelta(hours=5)
-        if now > promotion_time_5am:
-            promotion_time_5am += timedelta(days=1)
         good_night_time = datetime.combine(today, datetime.min.time()) + timedelta(hours=23,minutes=30)
         if now > good_night_time:
             good_night_time += timedelta(days=1)
-        next_message_time = min(good_morning_time, promotion_time_12pm, promotion_time_3pm, promotion_time_7pm, promotion_time_11pm, good_night_time, promotion_time_2am, promotion_time_5am)
+        next_message_time = min(good_morning_time, promotion_time_12pm, promotion_time_7pm, good_night_time)
         time_to_sleep = (next_message_time - now).total_seconds()
         print(f"Sleeping until: {next_message_time.strftime('%Y-%m-%d %H:%M:%S')} ({time_to_sleep} seconds)")
         await asyncio.sleep(time_to_sleep)
@@ -122,111 +110,7 @@ async def send_daily_messages():
                         icon_url="https://www.gstatic.com/youtube/img/branding/favicon/favicon_144x144_v2.png"
                     )
                     await channel.send(embed=embed)
-                elif next_message_time == promotion_time_3pm:
-                    embed = discord.Embed(
-                        title="Don't Miss Out on DRAXITY!",
-                        description=(
-                            "🚨 It's time for some epic gaming with DRAXITY! We've got all the highlights, action, "
-                            "and exciting content just for you! 🚨\n\n"
-                            "✨ **What We Offer:**\n"
-                            "🎮 Gaming Highlights\n"
-                            "🔥 Exciting Content\n"
-                            "🌀 Stay Connected with Our Community\n\n"
-                            "💡 **Want to join the squad?** Click below and get ready to play! 👇\n\n"
-                            "[**Join DRAXITY**](https://www.youtube.com/@DRAXITY_official)"
-                        ),
-                        color=0xFF0A01
-                    )
-                    embed.set_thumbnail(
-                        url="https://yt3.ggpht.com/DucAft3ewcWkcpILirmBDUSzvPaEo76jq2fQaUJiSayOKHJajN9-4i-IRPKj_Ivr3UGTGkS3pyQ=s108-c-k-c0x00ffffff-no-rj"
-                    )
-                    embed.set_image(
-                        url="https://yt3.googleusercontent.com/T8mYPeK07QbEKjq6pvG6_5CrFgnmAoI9K59lKqqZZai4pfuAYqlNqoDIVZ-0CJ17ATZugcvAaw=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
-                    )
-                    embed.set_footer(
-                        text="YouTube",
-                        icon_url="https://www.gstatic.com/youtube/img/branding/favicon/favicon_144x144_v2.png"
-                    )
-                    await channel.send(embed=embed)
                 elif next_message_time == promotion_time_7pm:
-                    embed = discord.Embed(
-                        title="Don't Miss Out on DRAXITY!",
-                        description=(
-                            "🚨 It's time for some epic gaming with DRAXITY! We've got all the highlights, action, "
-                            "and exciting content just for you! 🚨\n\n"
-                            "✨ **What We Offer:**\n"
-                            "🎮 Gaming Highlights\n"
-                            "🔥 Exciting Content\n"
-                            "🌀 Stay Connected with Our Community\n\n"
-                            "💡 **Want to join the squad?** Click below and get ready to play! 👇\n\n"
-                            "[**Join DRAXITY**](https://www.youtube.com/@DRAXITY_official)"
-                        ),
-                        color=0xFF0A01
-                    )
-                    embed.set_thumbnail(
-                        url="https://yt3.ggpht.com/DucAft3ewcWkcpILirmBDUSzvPaEo76jq2fQaUJiSayOKHJajN9-4i-IRPKj_Ivr3UGTGkS3pyQ=s108-c-k-c0x00ffffff-no-rj"
-                    )
-                    embed.set_image(
-                        url="https://yt3.googleusercontent.com/T8mYPeK07QbEKjq6pvG6_5CrFgnmAoI9K59lKqqZZai4pfuAYqlNqoDIVZ-0CJ17ATZugcvAaw=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
-                    )
-                    embed.set_footer(
-                        text="YouTube",
-                        icon_url="https://www.gstatic.com/youtube/img/branding/favicon/favicon_144x144_v2.png"
-                    )
-                    await channel.send(embed=embed)
-                elif next_message_time == promotion_time_11pm:
-                    embed = discord.Embed(
-                        title="Don't Miss Out on DRAXITY!",
-                        description=(
-                            "🚨 It's time for some epic gaming with DRAXITY! We've got all the highlights, action, "
-                            "and exciting content just for you! 🚨\n\n"
-                            "✨ **What We Offer:**\n"
-                            "🎮 Gaming Highlights\n"
-                            "🔥 Exciting Content\n"
-                            "🌀 Stay Connected with Our Community\n\n"
-                            "💡 **Want to join the squad?** Click below and get ready to play! 👇\n\n"
-                            "[**Join DRAXITY**](https://www.youtube.com/@DRAXITY_official)"
-                        ),
-                        color=0xFF0A01
-                    )
-                    embed.set_thumbnail(
-                        url="https://yt3.ggpht.com/DucAft3ewcWkcpILirmBDUSzvPaEo76jq2fQaUJiSayOKHJajN9-4i-IRPKj_Ivr3UGTGkS3pyQ=s108-c-k-c0x00ffffff-no-rj"
-                    )
-                    embed.set_image(
-                        url="https://yt3.googleusercontent.com/T8mYPeK07QbEKjq6pvG6_5CrFgnmAoI9K59lKqqZZai4pfuAYqlNqoDIVZ-0CJ17ATZugcvAaw=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
-                    )
-                    embed.set_footer(
-                        text="YouTube",
-                        icon_url="https://www.gstatic.com/youtube/img/branding/favicon/favicon_144x144_v2.png"
-                    )
-                    await channel.send(embed=embed)
-                elif next_message_time == promotion_time_2am:
-                    embed = discord.Embed(
-                        title="Don't Miss Out on DRAXITY!",
-                        description=(
-                            "🚨 It's time for some epic gaming with DRAXITY! We've got all the highlights, action, "
-                            "and exciting content just for you! 🚨\n\n"
-                            "✨ **What We Offer:**\n"
-                            "🎮 Gaming Highlights\n"
-                            "🔥 Exciting Content\n"
-                            "🌀 Stay Connected with Our Community\n\n"
-                            "💡 **Want to join the squad?** Click below and get ready to play! 👇\n\n"
-                            "[**Join DRAXITY**](https://www.youtube.com/@DRAXITY_official)"
-                        ),
-                        color=0xFF0A01
-                    )
-                    embed.set_thumbnail(
-                        url="https://yt3.ggpht.com/DucAft3ewcWkcpILirmBDUSzvPaEo76jq2fQaUJiSayOKHJajN9-4i-IRPKj_Ivr3UGTGkS3pyQ=s108-c-k-c0x00ffffff-no-rj"
-                    )
-                    embed.set_image(
-                        url="https://yt3.googleusercontent.com/T8mYPeK07QbEKjq6pvG6_5CrFgnmAoI9K59lKqqZZai4pfuAYqlNqoDIVZ-0CJ17ATZugcvAaw=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
-                    )
-                    embed.set_footer(
-                        text="YouTube",
-                        icon_url="https://www.gstatic.com/youtube/img/branding/favicon/favicon_144x144_v2.png"
-                    )
-                    await channel.send(embed=embed)
-                elif next_message_time == promotion_time_5am:
                     embed = discord.Embed(
                         title="Don't Miss Out on DRAXITY!",
                         description=(
